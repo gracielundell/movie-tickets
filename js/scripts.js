664f9323492c073e32e23e6c314c11905913caa5
gracie lundell and taylor grohs <gracielundell@gmail.com>
2016-01-26 11:43:08 -0800

    Add total price logic

--- js/scripts.js
+++ js/scripts.js
@@ -13,10 +13,31 @@ $(document).ready(function() {
     var movie = $("select#movie").val();
     var time = $("select#time").val();
     var age = $("select#age").val();
-    var newTicket = new Ticket(movie, time, age)
-    $(".display").append("<li>" + movie + "</li>");
-    $(".display").append("<li> At " + time + " </li>");
-    $(".display").append("<li> For " + age + " </li>");
+    var newTicket = new Ticket(movie, time, age);
+    var total = 10;
+    if(age === "senior" && (time === "12:00pm" || time === "3:00pm")){
+      total -= 6;
+    } else if (age === "senior"){
+      total -= 4;
+    } else if (age === "student" && (time === "12:00pm" || time === "3:00pm")) {
+      total -= 4;
+    } else if (age === "student") {
+      total -= 2;
+    } else if (age === "adult" && (time === "12:00pm" || time === "3:00pm")) {
+      total -= 2;
+    } else if (age === "adult") {
+      total = total ;
+    } else {
+      alert("What were you thinking bringing a baby to the movie, you insensitive tool")
+      total += 5;
+    }
+
+    $(".display").empty();
+    $(".display").append("<li class='movieli'>" + movie + "</li>");
+    $(".display").append("<li class='timeli'> At " + time + " </li>");
+    $(".display").append("<li class='agel1'> For " + age + " </li>");
+    $(".display").append("<li> Total: $" + total + "</li>")
+
     event.preventDefault();
   });
 
